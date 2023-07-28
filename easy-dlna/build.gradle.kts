feb3240e8aa5977224887dce443287a84d3d04bd
bei <lzb19890920@sina.com>
2023-07-28 11:23:20 +0800

    解决3.1.3无法编译的问题

--- easy-dlna/build.gradle.kts
+++ easy-dlna/build.gradle.kts
@@ -36,11 +36,23 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    packagingOptions {
+        exclude("META-INF/beans.xml")
+    }
 }
 
 dependencies {
     junit()
     cling()
-    jetty()
-    servlet()
+//    jetty()
+//    servlet()
+    implementation(files("libs/javax.servlet-3.0.0.v201103241009.jar"))
+    implementation(files("libs/jetty-client-8.1.9.v20130131.jar"))
+    implementation(files("libs/jetty-continuation-8.1.9.v20130131.jar"))
+    implementation(files("libs/jetty-http-8.1.9.v20130131.jar"))
+    implementation(files("libs/jetty-io-8.1.9.v20130131.jar"))
+    implementation(files("libs/jetty-security-8.1.9.v20130131.jar"))
+    implementation(files("libs/jetty-server-8.1.9.v20130131.jar"))
+    implementation(files("libs/jetty-servlet-8.1.9.v20130131.jar"))
+    implementation(files("libs/jetty-util-8.1.9.v20130131.jar"))
 }
